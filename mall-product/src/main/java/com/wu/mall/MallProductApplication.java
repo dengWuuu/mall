efb0ac0621bfe12faa60233158ca04d7c2b849ac
Jackson_Wu <dengwu.wu@foxmail.com>
2022-07-29 22:19:01 +0800

    项目初始化

---
+++ mall-product/src/main/java/com/wu/mall/MallProductApplication.java
@@ -0,0 +1,16 @@
+package com.wu.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wu
+ */
+@SpringBootApplication
+public class MallProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallProductApplication.class, args);
+    }
+
+}
---
+++ mall-ware/src/main/java/com/wu/mall/MallWareApplication.java
@@ -0,0 +1,16 @@
+package com.wu.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wu
+ */
+@SpringBootApplication
+public class MallWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallWareApplication.class, args);
+    }
+
+}
