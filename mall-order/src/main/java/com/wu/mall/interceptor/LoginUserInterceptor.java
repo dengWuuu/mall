80317a9614cc1606986b8ef855b86da8995fe4a6
Jackson_Wu <dengwu.wu@foxmail.com>
2022-10-22 15:14:09 +0800

    调整结构

--- mall-order/src/main/java/com/wu/mall/interceptor/LoginUserInterceptor.java
+++ mall-order/src/main/java/com/wu/mall/interceptor/LoginUserInterceptor.java
@@ -10,12 +10,10 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @Component
 public class LoginUserInterceptor implements HandlerInterceptor {
-
-
     public static ThreadLocal<MemberRespVo> loginUser = new ThreadLocal<>();
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
@@ -24,21 +22,18 @@ public class LoginUserInterceptor implements HandlerInterceptor {
         AntPathMatcher antPathMatcher = new AntPathMatcher();
         boolean match = antPathMatcher.match("/order/order/status/**", uri);
         boolean match1 = antPathMatcher.match("/payed/notify", uri);
-        if(match || match1){
+        if (match || match1) {
             return true;
         }
-
-
         MemberRespVo attribute = (MemberRespVo) request.getSession().getAttribute(AuthServerConstant.LOGIN_USER);
-        if(attribute!=null){
+        if (attribute != null) {
             loginUser.set(attribute);
             return true;
-        }else {
+        } else {
             //没登录就去登录
-            request.getSession().setAttribute("msg","请先进行登录");
+            request.getSession().setAttribute("msg", "请先进行登录");
             response.sendRedirect("http://auth.mall.com/login.html");
             return false;
         }
-
     }
 }
